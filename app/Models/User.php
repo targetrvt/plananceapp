880a5c72b621bfc9dd256fea19d6f7e2fd499729
re-denissfibigs <deniss.fibigs@reatom.com>
2025-01-28 19:18:25 +0200

    added working profile avatar, removed filament info widget, added reset password(not set up)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\HasAvatar;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasAvatar
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar_url', // Add avatar_url to mass assignable attributes
     ];
 
     /**
@@ -44,5 +45,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the URL of the user's avatar for Filament.
+     *
+     * @return string|null
+     */
+    public function getFilamentAvatarUrl(): ?string
+    {
+        // Check if avatar_url is set
+        if ($this->avatar_url) {
+            // Generate the correct URL to the avatar file
+            return asset('storage/' . $this->avatar_url);
+        }
+
+        // Return a default avatar URL if none is set
+        return 'https://ui-avatars.com/api/?name=' . urlencode($this->name);
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('dashboard')
             ->path('dashboard')
             ->login()
+            ->passwordreset()
             ->registration()
             ->colors([
                 'primary' => Color::Amber,
@@ -44,7 +45,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
